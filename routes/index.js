60f4cbf9c8d52eaad4d05462b4d296868e898678
Marvin Ropmuang <marvin@hewkhao.com>
2024-09-22 23:31:40 +0530

    update stipe webhook and cors

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const webhook = require('../modules/webhook');
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 
 //   res.render('index', { title: 'Express' });
 // });
+
+router.get('/webhook/stripe', async (req, res, next) => {
+    webhook.StripeWebhook(req, res);
+});
+
+
 
 module.exports = router;
